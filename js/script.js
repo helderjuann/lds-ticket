cee633b798f6c3510d6c40495192b41187cbe9de
Heldinnn <124900375+helderjuann@users.noreply.github.com>
2023-08-07 17:09:12 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $('.cpf').mask('000.000.000-00', {reverse: true});
 $('.kmkm').mask('000.000.000', {reverse: true});
 $('.licence').mask('AAA-0A00');
+
+function disableTextS() {
+    $('body').css('-webkit-user-select','none');
+    $('body').css('-moz-user-select','none');
+    $('body').css('-ms-user-select','none');
+    $('body').css('-o-user-select','none');
+    $('body').css('user-select','none');
+}
+
+disableTextS();
